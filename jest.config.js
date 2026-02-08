810e3960477729abb7dfd85246e8138c88d4bae7
MuhammadUsmanGM <MuhammadUsmanGM@users.noreply.github.com>
2026-02-08 03:26:44 +0000

    feat: add jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  verbose: true,
+  moduleFileExtensions: ['js', 'json'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  // Add more configurations as the project grows
+};
